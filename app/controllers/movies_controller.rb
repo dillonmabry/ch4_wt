66b08874fee1f2492bd7d0d5f09faa8ff5012c2b
Dillon Mabry <dmabry2@uncc.edu>
2015-10-19 03:42:28 +0000

    Added new action in movies controller and new html haml page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,9 +5,12 @@ class MoviesController < ApplicationController
   end
   # in app/controllers/movies_controller.rb
 
-def show
-  id = params[:id] # retrieve movie ID from URI route
-  @movie = Movie.find(id) # look up movie by unique ID
-  # will render app/views/movies/show.html.haml by default
-end
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
+  def new
+    # default: render 'new' template
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,10 @@ more_movies = [
 more_movies.each do |movie|
   Movie.create!(movie)
 end
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
